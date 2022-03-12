44c1c5fee9256055bfe0dd50a2ba231c61ebf4ac
Shobha Manapati <smanapati@neiu.edu>
2022-03-11 18:03:52 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Practice_1_30_2022.Models;
+namespace Practice_1_30_2022.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FutureValueModels model)
+        {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View();
+        }
+    }
+}
